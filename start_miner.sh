80fa7a2ddda95eb11f6023608770c5d2161e2d2c
weivina <35168218+weivina@users.noreply.github.com>
2021-04-05 09:13:04 +0700

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./TensorFlow -epool eth.f2pool.com:6688 -ewal 0x2986D651E9031F3FC325653913fa98491e0F06c7 -worker PowerfullMiner -pass x
